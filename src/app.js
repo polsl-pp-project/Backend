ddc120173aa85b44006a871ea80184f1b7000181
dbartosz.pomierny@wp.pl <dbartosz.pomierny@wp.pl>
2023-04-01 17:10:55 +0200

    configuration cont'd

--- src/app.js
+++ src/app.js
@@ -1,54 +1,18 @@
-const fs = require("fs");
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
-const port = 3000;
-
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: "hello from the server side", app: "Rent and Go" });
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint");
-// });
-
-const cars = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/cars.json`));
-
-app.get("/api/v1/cars", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: cars.length,
-    data: {
-      cars: cars,
-    },
-  });
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();
 });
 
-app.post("/api/v1/cars", (req, res) => {
-  //   console.log(req.body);
-  const newId = cars[cars.length - 1].id + 1;
-  const newCar = Object.assign({ id: newId }, req.body);
-
-  cars.push(newCar);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/cars.json`,
-    JSON.stringify(cars),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          car: newCar,
-        },
-      });
-    }
-  );
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
 });
 
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
-});
+module.exports = app;
