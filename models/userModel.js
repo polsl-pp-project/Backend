47c55207948b4e5c4f5810554815e6de1b2b594c
Piotr Wilk <piotrix12@gmail.com>
2023-04-06 16:16:54 +0200

    added user model

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Enter your name'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Enter your last name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Enter your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Enter valid email'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Enter your password'],
+        minlength: 5, //minimalna dlugosc hasla (tutaj przykladowo 5 znaków)
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Confirm your password'],
+    },
+});
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
