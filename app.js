5ceeacc76cebc3ba25b1b815577ec78a0095e232
dbartoszp <dbartosz.pomierny@wp.pl>
2023-04-07 17:28:33 +0200

    signup as a new user

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    // console.log('middleware');
     next();
 });
 
--- models/carModel.js
+++ models/carModel.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
-    carBrand: String,
-    price: Number,
-    year: Number,
-    mileage: Number,
-    body: String,
-    fuelType: String,
-    transmission: String,
-    doors: Number,
-    color: String,
-    drive: String,
-    power: Number,
-    engineCapacity: Number,
-    trunkCapacity: Number,
-    fuelConsumption: Number,
-    description: String,
+    carBrand: String, //marka
+    price: Number, //cena za miesiac wynajmu
+    year: Number, //rocznik
+    mileage: Number, //przebieg
+    body: String, //rodzaj nadwozia
+    fuelType: String, //rodzaj paliwa
+    transmission: String, //skrzynia biegow
+    doors: Number, //ilosc drzwi
+    color: String, //kolor
+    drive: String, //naped
+    power: Number, //il koni mechanicznych
+    engineCapacity: Number, //pojemnosc silnika
+    trunkCapacity: Number, //pojemnosc bagaznika
+    fuelConsumption: Number, //spalanie
+    description: String, //krotki opis
 });
 const Car = mongoose.model('Car', carSchema);
 
