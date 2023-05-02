a7c52d1726aa2c350cccd22eccf0132c0d9e0517
Piotr Wilk <piotrix12@gmail.com>
2023-05-02 22:15:51 +0200

    added "reset password" functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,10 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
+const sendEmail = require('./../utils/email');
 
 const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -112,10 +114,10 @@ exports.restrictTo = (...roles) => {
 exports.forgotPassword = catchAsync(async (req, res, next) => {
     //1) Get user based on POSTed email
 
-    const user = await User.findOne({ email: ewq.body.email });
+    const user = await User.findOne({ email: req.body.email });
 
     if (!user) {
-        return next(new AppError('email not found'), 404);
+        return next(new AppError('email not found', 404));
     }
 
     //2) Generate the random reset token
@@ -123,6 +125,63 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     //3)send email
+    const resetURL = `${req.protocol}://${req.get(
+        'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    const message = `Click here to reset your password ${resetURL}.\n If u didn't forget your password, ignore this message`;
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Your password reset token is valid for 10 min',
+            message,
+        });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email!',
+        });
+    } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        return next(
+            new AppError(
+                'There was an error sending the email. Try again later'
+            ),
+            500
+        );
+    }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+    //1)Get user based on token
+    const hashedToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest('hex');
+
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() },
+    });
+    //2)Check
+    if (!user) {
+        return next(new AppError('Token has expired or is incorrect', 400));
+    }
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    //3)Log in
+
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        token,
+    });
+});
