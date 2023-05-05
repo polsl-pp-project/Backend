fda3ba8fb1d1dcae671070e8de313067e1fada02
dbartoszp <dbartosz.pomierny@gmail.com>
2023-05-05 17:59:21 +0200

    crud car by number

--- controllers/carController.js
+++ controllers/carController.js
@@ -3,89 +3,147 @@ const Car = require('./../models/carModel');
 // const cars = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/cars.json`));
 
 exports.getAllCars = async (req, res) => {
-    try {
-        const cars = await Car.find();
+	try {
+		const cars = await Car.find();
 
-        res.status(200).json({
-            status: 'success',
-            results: cars.length,
-            data: {
-                cars,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err,
-        });
-    }
+		res.status(200).json({
+			status: 'success',
+			results: cars.length,
+			data: {
+				cars,
+			},
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
 };
 
 exports.getCar = async (req, res) => {
-    try {
-        const car = await Car.findById(req.params.id);
+	try {
+		const car = await Car.findById(req.params.id);
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                car,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err,
-        });
-    }
+		res.status(200).json({
+			status: 'success',
+			data: {
+				car,
+			},
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
+};
+
+exports.getCarByNumber = async (req, res) => {
+	try {
+		const car = await Car.findOne({ number: req.params.number });
+
+		res.status(200).json({
+			status: 'success',
+			data: {
+				car,
+			},
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
 };
 
 exports.createCar = async (req, res) => {
-    try {
-        const newCar = await Car.create(req.body);
+	try {
+		const newCar = await Car.create(req.body);
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                car: newCar,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: 'invalid data sent',
-        });
-    }
+		res.status(201).json({
+			status: 'success',
+			data: {
+				car: newCar,
+			},
+		});
+	} catch (err) {
+		res.status(400).json({
+			status: 'fail',
+			message: 'invalid data sent',
+		});
+	}
 };
 exports.updateCar = async (req, res) => {
-    try {
-        const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        res.status(200).json({
-            status: 'success',
-            data: {
-                car,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err,
-        });
-    }
+	try {
+		const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		res.status(200).json({
+			status: 'success',
+			data: {
+				car,
+			},
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
 };
+
+exports.updateCarByNumber = async (req, res) => {
+	try {
+		const car = await Car.findOneAndUpdate(
+			{ number: req.params.number },
+			req.body,
+			{
+				new: true,
+				runValidators: true,
+				useFindAndModify: false, // to avoid a deprecation warning
+			}
+		);
+		res.status(200).json({
+			status: 'success',
+			data: {
+				car,
+			},
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
+};
+
 exports.deleteCar = async (req, res) => {
-    try {
-        await Car.findByIdAndDelete(req.params.id);
-        res.status(204).json({
-            status: 'success',
-            data: null,
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err,
-        });
-    }
+	try {
+		await Car.findByIdAndDelete(req.params.id);
+		res.status(204).json({
+			status: 'success',
+			data: null,
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
+};
+exports.deleteCarByNumber = async (req, res) => {
+	try {
+		await Car.findOneAndDelete({ number: req.params.number });
+		res.status(204).json({
+			status: 'success',
+			data: null,
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
 };
--- models/carModel.js
+++ models/carModel.js
@@ -1,23 +1,42 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
-    carBrand: String, //marka
-    price: Number, //cena za miesiac wynajmu
-    year: Number, //rocznik
-    mileage: Number, //przebieg
-    body: String, //rodzaj nadwozia
-    fuelType: String, //rodzaj paliwa
-    transmission: String, //skrzynia biegow
-    doors: Number, //ilosc drzwi
-    color: String, //kolor
-    drive: String, //naped
-    power: Number, //il koni mechanicznych
-    engineCapacity: Number, //pojemnosc silnika
-    trunkCapacity: Number, //pojemnosc bagaznika
-    fuelConsumption: Number, //spalanie
-    description: String, //krotki opis
-    car_image: String,
+	number: Number,
+	carBrand: String, //marka
+	price: Number, //cena za miesiac wynajmu
+	year: Number, //rocznik
+	mileage: Number, //przebieg
+	body: String, //rodzaj nadwozia
+	fuelType: String, //rodzaj paliwa
+	transmission: String, //skrzynia biegow
+	doors: Number, //ilosc drzwi
+	color: String, //kolor
+	drive: String, //naped
+	power: Number, //il koni mechanicznych
+	engineCapacity: Number, //pojemnosc silnika
+	trunkCapacity: Number, //pojemnosc bagaznika
+	fuelConsumption: Number, //spalanie
+	description: String, //krotki opis
+	car_image: String,
 });
+
+carSchema.pre('save', async function (next) {
+	if (!this.isNew) {
+		return next();
+	}
+	const latestCar = await this.constructor.findOne(
+		{},
+		{},
+		{ sort: { number: -1 } }
+	);
+	if (!latestCar) {
+		this.number = 1;
+	} else {
+		this.number = latestCar.number + 1;
+	}
+	next();
+});
+
 const Car = mongoose.model('Car', carSchema);
 
 module.exports = Car;
